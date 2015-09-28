b5d2039c05383b29043ee6b2ba9ae53076eea730
anand.gupta <anand.gupta@netent.com>
2015-09-28 14:33:28 +0200

    My Second Commit.

--- src/main/java/com/sam/poc/HomeController.java
+++ src/main/java/com/sam/poc/HomeController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * Handles requests for the application home page.
+ * @Author - Anand Gupta
  */
 @Controller
 public class HomeController {
