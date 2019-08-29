a6c3a26fc7b8be326c8ec3b79d5b5e55edccdadd
Anand Gupta <mmt8132@mmt-mac-8132.local>
2019-08-29 14:24:51 +0530

    modified test2

--- src/main/java/com/sam/poc/Test2.java
+++ src/main/java/com/sam/poc/Test2.java
@@ -6,4 +6,9 @@ public class Test2 {
 		System.out.println("Test 2 constructor...");
 	}
 	
+	// added paramterized constructor
+	public Test2(int no) {
+		System.out.println("added parameterized constructor - int input - Test2");
+	}
+	
 }
