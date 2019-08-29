6d5fda42984e111f4066e8c2e951d8554b0b0e2c
Anand Gupta <mmt8132@mmt-mac-8132.local>
2019-08-29 15:18:54 +0530

    added method testMethod_b2

--- src/main/java/com/sam/poc/Test2.java
+++ src/main/java/com/sam/poc/Test2.java
@@ -6,4 +6,9 @@ public class Test2 {
 		System.out.println("Test 2 constructor...");
 	}
 	
+	// added through techpoint-b-2
+	public void testMethod_b2() {
+		System.out.println("added through techpoint_b_2");
+	}
+	
 }
